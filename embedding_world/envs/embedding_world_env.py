be052fb2dbdf2591287edd5312f337d799c30c5e
Samir <samir.moustafa.97>
2018-12-14 02:29:03 +0200

    Demo

--- embedding_world/envs/embedding_world_env.py
+++ embedding_world/envs/embedding_world_env.py
@@ -2,18 +2,59 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-class EmbeddingEnv(gym.Env):
 
-    #metadata = {'render.modes': ['human']}
+class EmbeddingEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        ...
+        pass
 
-    def step(self, action):
-        ...
+    def _step(self, action):
+        """
 
-    def reset(self):
-        ...
+        Parameters
+        ----------
+        action :
 
-    def render(self, mode='human', close=False):
-        ...
+        Returns
+        -------
+        ob, reward, episode_over, info : tuple
+            ob (object) :
+                an environment-specific object representing your observation of
+                the environment.
+            reward (float) :
+                amount of reward achieved by the previous action. The scale
+                varies between environments, but the goal is always to increase
+                your total reward.
+            episode_over (bool) :
+                whether it's time to reset the environment again. Most (but not
+                all) tasks are divided up into well-defined episodes, and done
+                being True indicates the episode has terminated. (For example,
+                perhaps the pole tipped too far, or you lost your last life.)
+            info (dict) :
+                 diagnostic information useful for debugging. It can sometimes
+                 be useful for learning (for example, it might contain the raw
+                 probabilities behind the environment's last state change).
+                 However, official evaluations of your agent are not allowed to
+                 use this for learning.
+        """
+        pass
+
+    def _reset(self):
+        pass
+
+    def _render(self, mode='human', close=False):
+        pass
+
+    def _take_action(self, action):
+        pass
+
+    def _get_reward(self):
+        pass
+
+    def _get_seed(self):
+        pass
+
+    def _get_close(self):
+        pass
+

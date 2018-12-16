dee258908a44c5d80ac0a309b0027af031edf5b0
Samir <samir.moustafa.97>
2018-12-16 09:30:30 +0200

    Initial commit

--- embedding_world/envs/embedding_world_env.py
+++ embedding_world/envs/embedding_world_env.py
@@ -4,78 +4,95 @@ import numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-from gensim.models import KeyedVectors
+from embedding_world.envs.embedding_world_handler import SpaceHandler
 
 class EmbeddingEnv(gym.Env):
     metadata = {'render.modes': ['human', "rgb_array"]}
 
-    ACTION = ["N", "S", "E", "W"]
+    ACTION = []
 
-    def __init__(self,embedding_file=None):
+    def __init__(self,embedding_file=None, epsilon=None):
 
-        # load the corpus to gensim model as word to vector
-        word2vec = KeyedVectors.load_word2vec_format(embedding_file, binary=False)
-        # get the embedding dimension
-        self.emb_dim = word2vec.wv.vector_size
+        if embedding_file and epsilon:
+            # load the corpus to gensim model as word to vector
+            self.space = SpaceHandler(space_file_path=embedding_file,epslion=0.0015)
+            # get the embedding dimension
+            self.emb_dim = self.space.emb_dim
+
+            # define pickup and drop down actions
+            self.ACTION.append(["pickup","dropout"])
+            # make two action for every dimension example : up and down == dim(0)+1 dim(0)-1
+            [self.ACTION.append(["dim(%s)+1" % dim, "dim(%s)-1" % dim]) for dim in range(self.emb_dim)]
+            # flatten 2D list of action to 1D list : len len(ACTION) = 2N + 2
+            self.ACTION = [j for sub in self.ACTION for j in sub]
+            # store epsilon inverse as a space size
+            self.space_size = int(epsilon**(-1))
+
+            # define the n-dimension space size
+            self.n_dim_space = tuple([self.space_size for i in range(self.emb_dim)])
+
+            # forward or backward in each dimension
+            self.action_space = spaces.Discrete(2 * len(self.n_dim_space) + 2)
+
+            low_i = []
+            high_i = []
+            # observation is the n-space
+            for i in range(self.emb_dim):
+                low_i.append(np.zeros(len(self.n_dim_space), dtype=int))
+                high_i.append(np.array(self.n_dim_space, dtype=int) - np.ones(len(self.n_dim_space), dtype=int))
 
 
-        '''
-        if embedding_file:
-            self.embedding = MazeView2D(maze_name="OpenAI Gym - Maze (%s)" % maze_file,
-                                        maze_file_path=maze_file,
-                                        screen_size=(640, 640))
-        elif maze_size:
-            if mode == "plus":
-                has_loops = True
-                num_portals = int(round(min(maze_size) / 3))
-            else:
-                has_loops = False
-                num_portals = 0
+            self.observation_space = spaces.Box(np.array(low_i), np.array(high_i))
 
-            self.maze_view = MazeView2D(maze_name="OpenAI Gym - Maze (%d x %d)" % maze_size,
-                                        maze_size=maze_size, screen_size=(640, 640),
-                                        has_loops=has_loops, num_portals=num_portals)
+            # initial condition
+            self.state = None
+            self.steps_beyond_done = None
+
+            # Simulation related variables.
+            self.seed()
+            self.reset()
+
+            # Just need to initialize the relevant attributes
+            #self._configure()
+
+            print('space size is epsilon inverse %i'%self.space_size)
+            print('list of all actions %s'%self.ACTION)
+            print(self.n_dim_space)
+            print(self.action_space)
+            print(self.observation_space)
+
         else:
-            raise AttributeError("One must supply either a maze_file path (str) or the maze_size (tuple of length 2)")
+            if epsilon is None:
+                raise AttributeError("must supply epsilon as float")
+            if embedding_file is None:
+                raise AttributeError("must supply embedding_file path as (str)")
 
-        self.maze_size = self.maze_view.maze_size
-
-        #raise FileNotFoundError("Cannot find %s." % embedding_file)
-        '''
-        pass
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
     def step(self, action):
-        """
-        Parameters
-        ----------
-        action :
+        if isinstance(action, int):
+            self.space.move_robot(self.ACTION[action])
+        else:
+            self.space.move_robot(action)
 
-        Returns
-        -------
-        ob, reward, episode_over, info : tuple
-            ob (object) :
-                an environment-specific object representing your observation of
-                the environment.
-            reward (float) :
-                amount of reward achieved by the previous action. The scale
-                varies between environments, but the goal is always to increase
-                your total reward.
-            episode_over (bool) :
-                whether it's time to reset the environment again. Most (but not
-                all) tasks are divided up into well-defined episodes, and done
-                being True indicates the episode has terminated. (For example,
-                perhaps the pole tipped too far, or you lost your last life.)
-            info (dict) :
-                 diagnostic information useful for debugging. It can sometimes
-                 be useful for learning (for example, it might contain the raw
-                 probabilities behind the environment's last state change).
-                 However, official evaluations of your agent are not allowed to
-                 use this for learning.
-        """
-        raise NotImplementedError
+        # TODO : Handle Goal in the handler class
+        if np.array_equal(self.space.robot, self.space.goal):
+            reward = 1
+            done = True
+        else:
 
-    def seed(self, **kwargs):
-        pass
+            # TODO : Handle punishment
+            reward = -0.1 / (self.space_size[0] * self.space_size[1])
+            done = False
+
+        self.state = self.space.robot
+
+        info = {}
+
+        return self.state, reward, done, info
+
 
     def reset(self):
         pass
@@ -105,4 +122,4 @@ class EmbeddingEnvExample(EmbeddingEnv):
 
     def __init__(self):
         print(os.getcwd())
-        super(EmbeddingEnvExample, self).__init__(embedding_file="embedding_world/envs/world_sample/mini.wiki.multi.en.vec")
+        super(EmbeddingEnvExample, self).__init__(embedding_file="embedding_world/envs/world_sample/mini.wiki.multi.en.vec", epsilon=0.006)
---
+++ embedding_world/envs/embedding_world_handler.py
@@ -0,0 +1,153 @@
+import os
+import random
+
+from gensim.models import KeyedVectors
+
+import numpy as np
+
+
+class SpaceHandler:
+    COMPASS = {}
+
+    def __init__(self, space_name="SpaceND", space_file_path=None, epslion=None, goal=None):
+
+        self.__goal = 0
+
+        self.__task_is_over = False
+
+        # Load a maze
+        if space_file_path is None:
+            self.__space = KeyedVectors.load_word2vec_format(space_file_path, binary=False)
+        else:
+            if not os.path.exists(space_file_path):
+                dir_path = os.path.dirname(os.path.abspath(__file__))
+                rel_path = os.path.join(dir_path, "world_samples", space_file_path)
+                if os.path.exists(rel_path):
+                    space_file_path = rel_path
+                else:
+                    raise FileExistsError("Cannot find %s." % space_file_path)
+            self.__space = KeyedVectors.load_word2vec_format(space_file_path, binary=False)
+
+        if epslion:
+            self.space_size = int(epslion ** (-1))
+        else:
+            raise ValueError("Epsilon can't be %s" % epslion)
+
+        self.emb_dim = self.__space.wv.vector_size
+
+        space_name = 'Space%iD' % self.emb_dim
+
+        # Set the starting point
+        self.__entrance = np.zeros(self.emb_dim, dtype=int)
+
+        if goal:
+            if not (len(goal) == self.emb_dim):
+                raise ValueError("Goal dimension must match with the space")
+            else:
+                # Set the Goal
+                self.__goal = np.array(goal)
+                # else:
+                # raise ValueError("Goal can't be %s"%goal)
+
+        self.COMPASS = {"dropout": tuple([1] * self.emb_dim),
+                        "pickup" : tuple([0] * self.emb_dim)}
+
+        temp_tuple = [0] * self.emb_dim
+        for i in range(self.emb_dim):
+
+            up, up[i] = temp_tuple, epslion
+            self.COMPASS["dim(%s)+1" % i] = tuple(up)
+
+            down, down[i] = temp_tuple, -1*epslion
+            self.COMPASS["dim(%s)-1" % i] = tuple(down)
+
+            temp_tuple = [0] * self.emb_dim
+
+        #for i, val in enumerate(self.COMPASS):
+        #    print(list(self.COMPASS.keys())[i], self.COMPASS.get(val))
+
+        # Create the Robot
+        self.__robot = self.entrance
+
+
+    def update(self, mode="human"):
+        try:
+            self.__controller_update()
+        except Exception as e:
+            self.__task_is_over = True
+            self.stop()
+            raise e
+        else:
+            return
+
+    def stop(self):
+        try:
+            self.__task_is_over = True
+        except Exception:
+            pass
+
+    def move_robot(self, dir):
+
+        if dir not in self.COMPASS.keys():
+            raise ValueError("dir cannot be %s. The only valid dirs are %s." % (str(dir), str(self.COMPASS.keys())))
+
+        if self.in_region(self.__robot, dir):
+
+            # move the robot
+            self.__robot += np.array(self.COMPASS[dir])
+            # if it's in a portal afterward
+            if self.space.is_portal(self.robot):
+                self.__robot = np.array(self.space.get_portal(tuple(self.robot)).teleport(tuple(self.robot)))
+
+    def in_region(self,robot, dir):
+        for i in self.__robot+np.array(self.COMPASS[dir]):
+            if (1 <= i or i <= -1): return False
+
+    def reset_robot(self):
+        self.__robot = np.zeros(self.emb_dim, dtype=int)
+
+    def __controller_update(self):
+        pass
+
+    @property
+    def space(self):
+        return self.__space
+
+    @property
+    def robot(self):
+        return self.__robot
+
+    @property
+    def entrance(self):
+        return self.__entrance
+
+    @property
+    def goal(self):
+        return self.__goal
+
+    @property
+    def game_over(self):
+        return self.__task_is_over
+
+
+class Portal:
+    def __init__(self, *locations):
+
+        self.__locations = []
+        for location in locations:
+            if isinstance(location, (tuple, list)):
+                self.__locations.append(tuple(location))
+            else:
+                raise ValueError("location must be a list or a tuple.")
+
+    def teleport(self, cell):
+        if cell in self.locations:
+            return self.locations[(self.locations.index(cell) + 1) % len(self.locations)]
+        return cell
+
+    def get_index(self, cell):
+        return self.locations.index(cell)
+
+    @property
+    def locations(self):
+        return self.__locations

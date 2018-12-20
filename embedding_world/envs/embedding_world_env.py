0e0e078ba12039322ca0ad610993c602678a528a
Samir <samir.moustafa.97>
2018-12-20 15:24:14 +0200

    Initial commit

--- embedding_world/envs/embedding_world_env.py
+++ embedding_world/envs/embedding_world_env.py
@@ -135,6 +135,6 @@ class EmbeddingEnv(gym.Env):
 class EmbeddingEnvExample(EmbeddingEnv):
     def __init__(self):
         super(EmbeddingEnvExample, self).__init__(
-            embedding_file="embedding_world/envs/world_sample/mini.wiki.multi.en.vec",
+            embedding_file="embedding_world/envs/world_sample/mini.wiki.multi.75.en.vec",
             epsilon=0.001
         )

c03e54f910382299abb4cc9690273e9aff4ebf77
Samir <samir.moustafa.97>
2019-01-20 20:16:57 +0200

    Initial commit

--- run_test.py
+++ run_test.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     env = gym.make('embedding_world-v0')
 
-    env.__set_sentences__('grab dia', 'ديه جرب')
+    env.__set_sentences__('grab dia', '')
 
     env.__set_paths__(embedding_from_file="embedding_world/envs/world_sample/mini.wiki.multi.2.en.vec",
                         embedding_to_file="embedding_world/envs/world_sample/mini.wiki.multi.2.ar.vec")
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     state, reward, done, info = env.step(np.array([0], dtype='int64'))
     print(state, reward, done, info)
     print("===================================")
-    state, reward, done, info = env.step(np.array([1], dtype='int64'))
+    state, reward, done, info = env.step(np.array([5], dtype='int64'))
     print(state, reward, done, info)
     print("===================================")
     state, reward, done, info = env.step(np.array([1], dtype='int64'))

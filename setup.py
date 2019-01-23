cec1b6450281673d26f98d34a6c5557d442ee974
Samir <samir.moustafa.97>
2019-01-23 02:18:31 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@
 from setuptools import setup, find_packages
 
 # read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.md') as f:
+      long_description = f.read()
 
 setup(name='gym-embedding_world',
-      version='0.0.3.3',
+      version='0.0.3.4',
       description='Two word embedding mapping compatible with OpenAI gym.',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       license="MIT",
       install_requires=['gym', 'numpy', 'gensim', 'atari-py'],
       keywords='embedding_world',
